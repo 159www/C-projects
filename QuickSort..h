6d33e37e1aae885b7e7c5ca8274eb18db64c120e
159www <70989276+159www@users.noreply.github.com>
2022-08-21 16:08:36 +0800

    Create QuickSort..h

---
+++ QuickSort..h
@@ -0,0 +1,10 @@
+//
+// Created by sgt on 2022/8/21.
+//
+
+#ifndef QUICKSORT2_0_QUICKSORT2_0_H
+#define QUICKSORT2_0_QUICKSORT2_0_H
+void QuickSort( int low,int high, int *nums);
+
+
+#endif //QUICKSORT2_0_QUICKSORT2_0_H
